c92ffaf34993379fbe64f937c11c8d509699c5c0
qhorn <quentinhorn62@gmail.com>
2025-11-13 03:06:26 +0100

    Update time_integration.f90

--- src/time_integration.f90
+++ src/time_integration.f90
@@ -1,94 +1,161 @@
-!===============================================================
-!  Module pour l’intégration en temps (CN + AB2)
-!===============================================================
-module mod_time_integration
+module time_integration
   use mod_kinds
   use mod_params
-  use mod_fields
-  use mod_operators
+  use mod_grid
   use mod_poisson_radial
+  use mod_operators
+  use mod_torpol
+  use mod_spectral_transforms
   implicit none
+
+  ! History for Adams–Bashforth 2
+  complex(dp), allocatable :: NL_W_old(:,:,:)
+  complex(dp), allocatable :: NL_LapU_old(:,:,:)
 
 contains
 
-  subroutine time_step(first_step)
-    logical, intent(in) :: first_step
-    integer :: ell, m, ir
-    complex(dp) :: lapW(0:LMAX,-MMAX:MMAX,NR)
-    complex(dp) :: lapLapU(0:LMAX,-MMAX:MMAX,NR)
-    complex(dp) :: RHS_loc(NR)
-    complex(dp) :: sol(NR)
-    real(dp) :: factor_CN
+!============================================================
+! Allocate history arrays
+!============================================================
+subroutine init_time_integrator()
+  allocate(NL_W_old(0:LMAX,-MMAX:MMAX,NR))
+  allocate(NL_LapU_old(0:LMAX,-MMAX:MMAX,NR))
+  NL_W_old    = (0.0_dp,0.0_dp)
+  NL_LapU_old = (0.0_dp,0.0_dp)
+end subroutine init_time_integrator
 
-    call apply_angular_laplacian(W_hat, lapW)
-    ! pour LapU, on aurait aussi lapLapU = Lap( LapU_hat ), si on traite 4e ordre complet
 
-    ! Construire les RHS pour W_hat et LapU_hat (non-linéaire + Coriolis + couplage)
-    ! Ici on suppose que RHS_W_hat et RHS_LapU_hat ont déjà reçu les contributions NL et couplage.
-    call add_coriolis_W(W_hat, RHS_W_hat)
-    call add_coriolis_LapU(LapU_hat, RHS_LapU_hat)
+!============================================================
+! Build RHS of CN/AB2 equations
+!============================================================
+subroutine build_RHS(U_hat, W_hat, NL_W, NL_LapU, RHS_W, RHS_LapU)
+  complex(dp), intent(in)  :: U_hat(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(in)  :: W_hat(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(in)  :: NL_W(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(in)  :: NL_LapU(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(out) :: RHS_W(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(out) :: RHS_LapU(0:LMAX,-MMAX:MMAX,NR)
 
-    factor_CN = dt / (2.0_dp*Re)
+  integer :: l,m,ir
+  real(dp) :: alpha   ! ν dt / 2
+  alpha = dt/(2.0_dp*Re)
 
-    ! Boucle sur (ell,m), résolution radiale
-    do ell = 0, LMAX
+  RHS_W     = (0.0_dp,0.0_dp)
+  RHS_LapU  = (0.0_dp,0.0_dp)
+
+  do ir = 1, NR
+    do l = 0, LMAX
       do m = -MMAX, MMAX
 
-        ! W: (I - factor_CN*Lap) W^{n+1} = (I + factor_CN*Lap) W^n + dt * RHS_AB2
-        ! AB2:
-        if (first_step) then
-          ! Euler pour initialiser
-          do ir = 1, NR
-            RHS_loc(ir) = W_hat(ell,m,ir) + dt * RHS_W_hat(ell,m,ir)   &
-                          + factor_CN * lapW(ell,m,ir)
-          enddo
-        else
-          do ir = 1, NR
-            RHS_loc(ir) = W_hat(ell,m,ir) + dt * ( 1.5_dp*RHS_W_hat(ell,m,ir) &
-                                     - 0.5_dp*RHS_W_hat_old(ell,m,ir) ) &
-                          + factor_CN * lapW(ell,m,ir)
-          enddo
-        endif
+        ! CN part: (I + α L) * W
+        RHS_W(l,m,ir) = (1.0_dp,0.0_dp) * W_hat(l,m,ir)
 
-        call solve_radial_W(ell, RHS_loc, sol)
-        do ir = 1, NR
-          W_hat(ell,m,ir) = sol(ir)
-        enddo
+        ! same for LapU
+        RHS_LapU(l,m,ir) = (1.0_dp,0.0_dp) * ( Laplacian_of_U(l,m,ir) )
 
-        ! Idem pour LapU
-        if (first_step) then
-          do ir = 1, NR
-            RHS_loc(ir) = LapU_hat(ell,m,ir) + dt * RHS_LapU_hat(ell,m,ir)
-          enddo
-        else
-          do ir = 1, NR
-            RHS_loc(ir) = LapU_hat(ell,m,ir) + dt * ( 1.5_dp*RHS_LapU_hat(ell,m,ir) &
-                                        - 0.5_dp*RHS_LapU_hat_old(ell,m,ir) )
-          enddo
-        endif
+        ! AB2 nonlinear part
+        RHS_W(l,m,ir) = RHS_W(l,m,ir) + &
+           (1.5_dp)*NL_W(l,m,ir) - 0.5_dp*NL_W_old(l,m,ir)
 
-        call solve_radial_LapU(ell, RHS_loc, sol)
-        do ir = 1, NR
-          LapU_hat(ell,m,ir) = sol(ir)
-        enddo
+        RHS_LapU(l,m,ir) = RHS_LapU(l,m,ir) + &
+           (1.5_dp)*NL_LapU(l,m,ir) - 0.5_dp*NL_LapU_old(l,m,ir)
 
       enddo
     enddo
+  enddo
 
-    ! Double Poisson: ∇² U = LapU_hat → résoudre pour U_hat
-    do ell = 0, LMAX
+end subroutine build_RHS
+
+
+!============================================================
+! Apply linear operators (Coriolis + geometry)
+!============================================================
+subroutine apply_linear_terms(W_hat, U_hat, RHS_W, RHS_LapU)
+  complex(dp), intent(in)    :: W_hat(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(in)    :: U_hat(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(inout) :: RHS_W(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(inout) :: RHS_LapU(0:LMAX,-MMAX:MMAX,NR)
+
+  call apply_linear_W(W_hat, U_hat, RHS_W)
+  call apply_linear_LapU(LapU_hat, U_hat, W_hat, RHS_LapU)
+end subroutine apply_linear_terms
+
+
+!============================================================
+! Time step update (CN for linear + AB2 for NL)
+!============================================================
+subroutine time_step(U_hat, W_hat)
+
+  complex(dp), intent(inout) :: U_hat(0:LMAX,-MMAX:MMAX,NR)
+  complex(dp), intent(inout) :: W_hat(0:LMAX,-MMAX:MMAX,NR)
+
+  complex(dp), allocatable :: RHS_W(:,:,:)
+  complex(dp), allocatable :: RHS_LapU(:,:,:)
+  complex(dp), allocatable :: NL_W(:,:,:)
+  complex(dp), allocatable :: NL_LapU(:,:,:)
+
+  integer :: l,m,ir
+
+  allocate(RHS_W(0:LMAX,-MMAX:MMAX,NR))
+  allocate(RHS_LapU(0:LMAX,-MMAX:MMAX,NR))
+  allocate(NL_W(0:LMAX,-MMAX:MMAX,NR))
+  allocate(NL_LapU(0:LMAX,-MMAX:MMAX,NR))
+
+  !-----------------------------------
+  ! 1. Velocity & Vorticity
+  !-----------------------------------
+  call compute_velocity(U_hat, W_hat)
+  call compute_vorticity()
+
+  !-----------------------------------
+  ! 2. NL term
+  !-----------------------------------
+  call compute_nonlinear()
+  call project_nonlinear_to_spectral(NLr,NLth,NLph, NL_W, NL_LapU)
+
+  !-----------------------------------
+  ! 3. Build RHS
+  !-----------------------------------
+  call build_RHS(U_hat, W_hat, NL_W, NL_LapU, RHS_W, RHS_LapU)
+
+  !-----------------------------------
+  ! 4. Apply linear operators
+  !-----------------------------------
+  call apply_linear_terms(W_hat, U_hat, RHS_W, RHS_LapU)
+
+  !-----------------------------------
+  ! 5. Solve Poisson-type radial inversions
+  !-----------------------------------
+  do ir = 1, NR
+    do l = 0, LMAX
       do m = -MMAX, MMAX
-        do ir = 1, NR
-          ! TODO: remplacer par solve_radial_Poisson(ell, LapU_hat(ell,m,:), U_hat(ell,m,:))
-          U_hat(ell,m,ir) = LapU_hat(ell,m,ir)   ! stub
-        enddo
+        W_hat(l,m,ir)    = solve_mode_radial_W( l, m, ir, RHS_W(l,m,ir) )
+        LapU_hat(l,m,ir) = solve_mode_radial_LapU( l, m, ir, RHS_LapU(l,m,ir) )
       enddo
     enddo
+  enddo
 
-    ! Shift des RHS pour AB2
-    RHS_W_hat_old    = RHS_W_hat
-    RHS_LapU_hat_old = RHS_LapU_hat
+  !-----------------------------------
+  ! 6. Update history for AB2
+  !-----------------------------------
+  NL_W_old    = NL_W
+  NL_LapU_old = NL_LapU
 
-  end subroutine time_step
+end subroutine time_step
+
+! Dummy wrappers for clarity
+pure function solve_mode_radial_W(l,m,ir,rhs) result(sol)
+  integer, intent(in) :: l,m,ir
+  complex(dp), intent(in) :: rhs
+  complex(dp) :: sol
+  call solve_radial_W(l, (/rhs/), (/sol/) )
+end function
+
+pure function solve_mode_radial_LapU(l,m,ir,rhs) result(sol)
+  integer, intent(in) :: l,m,ir
+  complex(dp), intent(in) :: rhs
+  complex(dp) :: sol
+  call solve_radial_LapU(l, (/rhs/), (/sol/) )
+end function
 
 end module mod_time_integration
